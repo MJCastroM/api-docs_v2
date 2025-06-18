ba9802b42f8d4cd0eb8ac712ff6be734606f7d5e
MJCastroM <42466543+MJCastroM@users.noreply.github.com>
2025-06-18 12:41:30 -0300

    Revert "Cambios esteticos"

--- src/app/features/api-docs/components/backend-config/backend-config.component.ts
+++ src/app/features/api-docs/components/backend-config/backend-config.component.ts
@@ -1,9 +1,8 @@
 import { Component, Input } from '@angular/core';
 
-@Component({ selector: 'app-backend-config', templateUrl: './backend-config.component.html',
+@Component({ selector: 'app-backend-config', templateUrl: './backend-config.component.html', 
   styleUrls: ['./backend-config.component.scss'] })
 export class BackendConfigComponent {
-  @Input() backendCols!: string[];
-  @Input() backendData!: any[];
+  @Input() config!: { campo: string; valor: string }[];
   @Input() text!: string
 }
