5323e5a7b50aea5fb1e9d3e79751812aad60b8f1
GabrielPontes04 <105681324+GabrielPontes04@users.noreply.github.com>
2025-07-03 22:33:43 -0300

    Se añade la opción del modo oscuro

--- src/app/layout/navbar/navbar.component.ts
+++ src/app/layout/navbar/navbar.component.ts
@@ -1,8 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent {}
+export class NavbarComponent implements OnInit {
+  isDarkMode = false;
+
+  ngOnInit() {
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme === 'dark') {
+      this.isDarkMode = true;
+      document.body.classList.add('dark-mode');
+    }
+  }
+
+  rotate = false;
+
+  toggleTheme() {
+    this.rotate = true;
+    setTimeout(() => this.rotate = false, 400);
+
+    this.isDarkMode = !this.isDarkMode;
+    document.body.classList.toggle('dark-mode');
+    localStorage.setItem('theme', this.isDarkMode ? 'dark' : 'light');
+  }
+
+}
